7031849f6fcde16efd740c2d16f5440e841ab75e
somatesttech <somatesttech@gmail.com>
2023-03-19 00:38:59 +0530

    removed default null from scid

--- models/user.model.js
+++ models/user.model.js
@@ -6,7 +6,7 @@ var userSchema = new mongoose.Schema({
   email: {type: String, lowercase: true, unique: true,sparse:true, required: [true, "can't be blank"], match: [/\S+@\S+\.\S+/, 'is invalid']},
   uid: {type: String, required: [true, "uid can't be blank"], index: true},
   //STRIPE CUSTOMER ID
-  s_cid: {type: String, default:null, unique: true,sparse:true, index: true},
+  s_cid: {type: String, unique: true,sparse:true, index: true},
   verified:{type:Boolean, default:false},
   has_trial:{type:Boolean, default:true},
   //STRIPE SUBSCRIPTION ID
